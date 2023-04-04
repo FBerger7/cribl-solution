a9475057657d6032de3301d5e4cf36e5fe05c030
Franciszek Berger <bergerfranek@gmail.com>
2023-04-04 22:46:30 +0200

    Add pytest, a mock testcase and requirements.txt

---
+++ shared/testsuites/performance/test_mock/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains a mock testcase
+"""
+
+
+class TestMock:
+
+    @staticmethod
+    def test_mock() -> None:
+        """
+        Performs a mock testcase
+        """
+        assert 2 == 2
